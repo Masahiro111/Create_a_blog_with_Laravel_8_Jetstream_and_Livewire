53ff176df45631bfa8f68302771c7fb778f167f2
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-01-20 19:14:38 +0900

    Revert "finished part 2"
    This reverts commit f3e02bda248d4541b79d9ce6e92b4841b50afbc4.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -59,9 +58,4 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
-
-    public function posts(): HasMany
-    {
-        return $this->hasMany(Post::class);
-    }
 }
